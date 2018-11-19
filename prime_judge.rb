cbef583df485658595b7fad8cb449ac34f881ca3
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2018-11-20 02:33:51 +0900

    2018/11/20 02:34:00

--- prime_judge.rb
+++ prime_judge.rb
@@ -1,22 +1,31 @@
 #!/usr/bin/ruby
 #メルセンヌ数2^13-1が素数であることを確かめるスクリプト
 
-val = 8191
+$val = 8191
 
-if (val % 2 == 0)
-	puts val.to_s + " is not a prime number."
-else
-	puts val.to_s + " cannot be devided by " + "2"
-	i = 3
-	while (i <= val.quo(2))
-		if (val % i == 0)
-			puts val.to_s + " can be devided by " + i.to_s + "\n" + val.to_s + " is not a prime number."
-			exit
-		else
-			puts val.to_s + " cannot be devided by " + i.to_s
-			i = i + 2
-		end	
+#エラトステネスの篩を実行するメソッド「prime_judgment」を定義。
+def prime_judgment num
+	if ($val % num == 0)
+		puts $val.to_s + " can be devided by " + num.to_s + "\n" + $val.to_s + " is not a prime number."
+		exit
+	else
+		puts $val.to_s + " cannot be devided by " + num.to_s
 	end
 end
 
-puts val.to_s + " is a prime number."
+#whileループを使って「prime_judgment」を回す。
+if ($val % 2 == 0)
+	puts val.to_s + " is not a prime number."
+else
+	puts $val.to_s + " cannot be devided by " + "2"
+	i = 3
+	while (i <= $val.quo(2))
+		prime_judgment(i)
+		i = i + 3
+	end
+end
+
+puts $val.to_s + " is a prime number."
+
+
+
