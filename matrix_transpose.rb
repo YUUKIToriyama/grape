98d0de2043fa65cb293fd7649e1db53de29d08bf
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-01-13 22:20:36 +0900

    minor modification

--- matrix_transpose.rb
+++ matrix_transpose.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/ruby
-# 3x3正方行列の転置
+# 4x3正方行列の転置
 
-vct_1 = [1,2,3]
-vct_2 = [4,5,6]
-vct_3 = [7,8,9]
+vct_1 = [1,2,3,4]
+vct_2 = [5,6,7,8]
+vct_3 = [9,10,11,12]
 
 mtrx = [vct_1, vct_2, vct_3]
 
 # 行列の例
 mtrx.each do |row|
-	puts row.join(",")
+	puts row.join("\t")
 end
 
 # 行列の転置
 mtrx_new = Array.new
 
-for i in 0..2
+for i in 0..3
 	tmp_arr = Array.new
 	for l in 0..2
 		tmp_arr.push(mtrx[l][i])
@@ -27,5 +27,5 @@ end
 
 # 転置した行列を表示
 mtrx_new.each do |row|
-	puts row.join(",")
+	puts row.join("\t")
 end
