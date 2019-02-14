e36c12a0d1926fb5d600e351e3e821c54bfe5980
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-02-14 12:00:05 +0900

    Add files via upload

--- bionomial_expansion.rb
+++ bionomial_expansion.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/ruby
+# 4を法としたパスカルの三角形はフラクタルを描きます
+
+
 # 二項展開に現れる係数を計算する
+arr = [[1]]
 
-arr = [[1,1]]
-
-for l in 1..20
+for l in 1..60
 	tmp = [1]
 	for m in 1..(arr[l-1].length - 1)
 		tmp.push(arr[l-1][m-1] + arr[l-1][m])
@@ -12,6 +14,10 @@ for l in 1..20
 	arr.push(tmp)
 end
 
-arr.each do |a|
-	puts a.join(",")
+# 各項を４で割り色付き絵文字に変換します
+arr.each do |row|
+	row.map! do |elm|
+		elm = (elm % 4).to_s.gsub("0", "👀").gsub("1", "🎀").gsub("2", "💧").gsub("3", "⭐")
+	end
+	puts row.join(",")
 end
