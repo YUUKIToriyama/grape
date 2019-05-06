cdedc3b2ef7e3af870dbd32e027f7bb17500ce33
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-05-06 11:11:44 +0900

    Update random_img.rb

--- imagemagick/random_img.rb
+++ imagemagick/random_img.rb
@@ -9,12 +9,6 @@ puts "# ImageMagick pixel enumeration: #{width},#{height},#{gradation},srgb"
 
 for n in 0..width-1
 	for m in 0..height-1
-		arr = []
-		3.times do
-			arr.push(rand(gradation))
-		end
-		print "#{n},#{m}: ("
-		print arr.join(",")
-		print ")\n"
+		print "#{n},#{m}: (#{rand(gradation)},#{rand(gradation)},#{rand(gradation)})\n"
 	end
 end
