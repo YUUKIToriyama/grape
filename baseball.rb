7129099144570ecdfe1a46f4ce70549f1e0d796b
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-05-23 09:23:52 +0900

    some fixes created

--- baseball.rb
+++ baseball.rb
@@ -1,30 +1,61 @@
 #!/usr/bin/ruby
-arr = []
-STDIN.map do |line|
-    arr.push(line.chomp)
-end
-num = arr.shift.to_i
+# BASEBALL GAME
+#
+# How to Play
+# type "s" or "b" or "f" or "d".
+# when you quit the game, type "q" or "ctrl + C"
+#
+# (c) Copyright 2019 YUUKIToriyama
 
+def print_score(s,b,o)
+	puts "S " + "🔶" * s
+	puts "B " + "🔵" * b
+	puts "O " + "❌" * o
+end
 
 c_st = 0
 c_ba = 0
+c_out = 0
 
-for i in 1..num
-    if arr[i - 1] == "strike"
-        c_st = c_st + 1
-        if c_st <= 2
-            puts "strike!"
-        else
-            puts "out!"
-            break
-        end
-    else
-        c_ba = c_ba + 1
-        if c_ba <= 3
-            puts "ball!"
-        else
-            puts "fourball!"
-            break
-        end
-    end
+while c_out < 3
+	judge = gets.chomp
+	if judge == "s"
+		c_st = c_st + 1
+		puts "Strike!"
+		if c_st == 3
+			puts "3 Strike, Batter Out!"
+			c_st = 0
+			c_ba = 0
+			c_out = c_out + 1
+		end
+		print_score(c_st,c_ba,c_out)
+	elsif judge == "b"
+		c_ba = c_ba + 1
+		puts "Ball!"
+		if c_ba == 4
+			puts "4 Ball, Runner Goes First Base!"
+			c_st = 0
+			c_ba = 0
+		end
+		print_score(c_st,c_ba,c_out)
+	elsif judge == "f"
+		puts "Foul Ball!"
+		if c_st <= 2
+			c_st = c_st + 1
+		end
+		print_score(c_st,c_ba,c_out)
+	elsif judge == "d"
+		puts "Dead Ball! Runner Goes First Base!"
+		c_st = 0
+		c_ba = 0
+		print_score(c_st,c_ba,c_out)
+	elsif judge == "q"
+		exit
+	else
+		puts "Please type s / b / f / d"
+		puts "s: Strike\nb: Ball\nf: Faul Ball\nd: Dead Ball"
+	end
 end
+
+puts "3 Out! Change."
+exit
--- shuf_array.rb
+++ shuf_array.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/ruby
 #配列の中身をシャッフルするメソッド「shuf_array」を作ります。厳密に言うと、配列からランダムに取り出した要素を別に用意した空の配列にpushするメソッドです。
 
-def shuf_array(arr)
-	#空の配列を用意します
-	tmp = Array.new
-	for i in 1..arr.length
-		choose_one = rand(arr.length)
-		tmp.push(arr[choose_one])
-		arr.delete_at(choose_one)
+class Array
+	def shuf_array
+		a = self
+		arr = []
+		while a.length > 0
+			r = rand(a.length)
+			arr.push(a.delete_at(r))
+		end
+		return arr
 	end
-	arr = tmp
 end
 
-
-# 例
+# 実行例
 animals = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
 
-# 実行
-puts shuf_array(animals)
+puts animals.shuf_array
