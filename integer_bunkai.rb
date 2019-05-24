06590c1861595e2430f46f0f1a84591a44612d50
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-05-24 19:00:03 +0900

    🔧some fixes

--- integer_bunkai.rb
+++ integer_bunkai.rb
@@ -1,9 +1,25 @@
 #!/usr/bin/ruby
-#自然数を桁ごとに分解します
+# 自然数を桁ごとに分解します
+
+
+# 整数の桁数を求めるメソッドInteger#lengthを定義
+class Integer
+	def length
+		if self == 0
+			return 1
+		else
+			c = 0
+			while 10 ** c <= self.abs
+				c = c + 1
+			end
+			return c
+		end
+	end
+end
 
 i = 23456876543 #一般の自然数を想定
 
-digits = i.to_s.length #桁数を求める
+digits = i.length #桁数を求める
 
 nums = Array.new
 for n in 1..digits do
@@ -11,5 +27,5 @@ for n in 1..digits do
 	nums.push (a) #とりあえず配列に入れておく
 end
 
-nums = nums.reverse #小さい順になっているので逆にしておく
-puts nums.join("\n")
+#小さい順になっているので逆にして出力
+puts nums.reverse.join(",")
