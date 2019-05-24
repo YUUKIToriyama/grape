8fdeda6e0f332d8c916a36cff7955b9c2e1fcdff
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-05-24 19:22:21 +0900

    2019年  5月 24日 金曜日 19:22:21 JST

--- integer_bunkai.rb
+++ integer_bunkai.rb
@@ -2,8 +2,8 @@
 # 自然数を桁ごとに分解します
 
 
-# 整数の桁数を求めるメソッドInteger#lengthを定義
 class Integer
+	# 整数の桁数を求めるメソッドInteger#lengthを定義
 	def length
 		if self == 0
 			return 1
@@ -15,12 +15,35 @@ class Integer
 			return c
 		end
 	end
+	# 自然数を桁区切りに10^nの大きさごとに分解した配列を返すメソッドInteger#to_ketakugiriを定義
+	def to_ketakugiri
+		num = self.abs
+		arr = Array.new
+
+		c = 1
+		while c <= num.length
+			m = num % 10 ** c
+			arr.push(m)
+			num = num - m
+			c = c + 1
+		end
+		arr.reverse!
+		if self < 0
+			return arr.map{|n| n * (-1)}
+		else
+			return arr
+		end
+	end
 end
 
-i = 23456876543 #一般の自然数を想定
+i = 114514 #一般の自然数を想定
 
-digits = i.length #桁数を求める
+puts i.length #桁数を求める
+puts i.to_ketakugiri.join("+") #桁区切りにして出力
 
+
+
+=begin
 nums = Array.new
 for n in 1..digits do
 	a = i % (10 ** n) - i % (10 ** (n - 1)) #剰余を利用して分解する
@@ -29,3 +52,4 @@ end
 
 #小さい順になっているので逆にして出力
 puts nums.reverse.join(",")
+=end
--- number_game.rb
+++ number_game.rb
@@ -1,20 +1,26 @@
 #!/usr/bin/ruby
 # 年齢あてゲーム
+#
+# 16~25歳の範囲で年齢を当てるゲームです。まだ若いから失礼じゃない！？
 
 num = 25 - rand(10)
 
-
-while (1 == 1)
-	ans = gets.chomp!.to_i
-	if (ans == num)
-		puts "見事的中！私は#{num}歳です"
+while true
+	if gets.chomp == "q"
+		puts "終了します"
 		exit
-	elsif (ans > num)
+	elsif gets.class != Integer	
+		puts "半角数字を入力してね"
+	else
+		ans = gets.to_i
+	end
+
+	if (ans > num)
 		puts "もう少し若いです"
 	elsif (ans < num)
 		puts "もう少し年上ですよ"
 	else
-		puts "半角数字を入力してね"
+		puts "見事的中！私は#{num}歳です"
+		exit
 	end
 end
-
