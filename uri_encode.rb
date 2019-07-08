9ba12a91722d01ff82288ce1f8ff9e496c6dbeba
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-07-08 16:18:24 +0900

    1562570304

--- uri_encode.rb
+++ uri_encode.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/ruby
+# FlickrAPIアクセス用
 
 require 'uri'
 
-uri = URI.parse("https://example.com/search?q=yamada")
+API_KEY = "123456789abcde0"
+uri = URI.parse("https://api.flickr.com/services/rest")
 
 query_arr = [
-	["q", "rubyチートシート"],
-	["utf8", "yes"],
-	["browser", "firefox"],
-	["num", 113]
+	["api_key", API_KEY],
+	["method", "flickr.photos.search"],
+	["tags", "kyoto,hushimi"],
+	["has_geo", 1],
+	["extras", "geo,url_h,date_taken"],
+	["format", "json"],
+	["nojsoncallback", 1]
 ]
 
 uri.query = URI.encode_www_form(query_arr.to_h)
