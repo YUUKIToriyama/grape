18495ebe7a69d4183f476bb7627f62f6fc711f3d
ytoriyama <ytoriyama@torisfactory.com>
2019-11-27 01:22:58 +0900

    game.rb: 盤面を乱数を用いて自動生成するようにした

--- game.rb
+++ game.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
 # 二次元ルービックキューブ
 
-matrix = [
-	[3, 2, 1],
-	[4, 5, 6],
-	[7, 8, 9]
-]
+#matrix = [
+#	[3, 2, 1],
+#	[4, 5, 6],
+#	[7, 8, 9]
+#]
+
+def create_new_matrix
+	arr = (1..9).to_a.shuffle
+	return [arr.slice(0, 3), arr.slice(3, 3), arr.slice(6, 3)]
+end
+
 
 class Array
 	def horizontal_move(row_n, m)
@@ -31,6 +37,8 @@ class Array
 		return (self == correct_ans) ? true : false
 	end
 end
+
+matrix = create_new_matrix()
 
 matrix.each{|l| puts l.join(" ")}
 how_to_play = "How to play\nrotate Row1 2 times, then type 'r 1 2'\nrotate Column3 at once, then type 'c 3 1"
